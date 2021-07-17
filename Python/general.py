a1daecae04be5e7eceb69b2c6a664a4ceb8d6011
Troy <troyd9216@gmail.com>
2021-07-17 18:13:26 -0500

    added general and props

---
+++ Python/general.py
@@ -0,0 +1,20 @@
+# the class keyword tells the compiler that we are creating
+# a custom class
+class ClassName:
+    #classes must
+    #! 1) start with a letter or underscore
+    #! 2) Should only include Numbers, lettesr, or underscores
+    pass
+#all of the properties and methods will be defined within
+# the classes scope
+
+obj = myClass() # myClass is used to instantiate an
+# object of the class
+
+# python does distinguish the difference between invoking a function and
+# and instantiating a class
+
+# print(obj)
+#printing this object will show the memory address at which the object is located
+
+#? output = <__main__.MyClass object at 0x7f6db7fd2978>
