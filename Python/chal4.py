dd20426cd787e74d7a63ea0e9c7f9236edea7889
Troy <troyd9216@gmail.com>
2021-07-19 17:16:26 -0500

    chal4

---
+++ Python/chal4.py
@@ -0,0 +1,23 @@
+# Problem statement #
+# You are given a partially completed code of a Rectangle class in the editor. Implement the class by completing the tasks below.
+
+# Task 1 #
+# Implement a constructor to initialize the values of two private properties: length and width.
+
+# Task 2 #
+# Implement a method, area(), in the Rectangle class that returns the product of length and width. See the formula below:
+
+# Area = length \times widthArea=length×width
+
+class Rectangle:
+    def __init__(self, length=None, width=None):
+        self.__length = length
+        self.__width = width
+
+    def area(self):
+        area = self.__length * self.__width
+        return area
+
+    def perimeter(self):
+        perimeter = 2 * (self.__length + self.__width)
+        return perimeter
