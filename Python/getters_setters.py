2111e7138d2fc0f6ec857f576191c3948a154e4e
Troy <troyd9216@gmail.com>
2021-07-19 16:52:47 -0500

    getters+setters

---
+++ Python/getters_setters.py
@@ -0,0 +1,23 @@
+# Getter mehtods allow reading a properties values
+# Setters allows modifying a properties value
+
+class User:
+    def __init__(self, username=None):
+        # definie the initializer
+        self.__username = username
+
+    # defining the setter
+    def setUsername(self, x):
+        # defining self as the value passed in as x
+        self.__username = x
+
+    # defining the getter
+    def getUsername(self):
+        # simply returning the private variable username
+        return (self.__username)
+
+
+Steve = User('steve1')
+print('Before setting:', Steve.getUsername())
+Steve.setUsername('steve2')
+print('After setting:', Steve.getUsername())
