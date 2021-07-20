013054478fbd57c26bfacf6c43d6675f17236dd0
Troy <troyd9216@gmail.com>
2021-07-19 22:41:31 -0500

    chal

--- Python/chal6.py
+++ Python/chal6.py
@@ -5,9 +5,21 @@ class Account:
         self.title = title
         self.balance = balance
 
+        def getBalance(self):
+            return self.balance
+
+        def deposit(self, amount):
+            self.balance = self.balance + amount
+
+        def withdrawl(self, amount):
+            self.balance = self.balance - amount
+
 class SavingsAccount(Account):
+    # __init__ is the Initializer!
     def __init__(self, title=None, balance=0, interestRate=0):
+        # super calls the initializer in the parent class
         super().__init__(title, balance)
         self.interestRate = interestRate
 
-a1 = Account("Mark", 5000)
+    def interestAmount(self):
+        return (self.balance * self.interestRate / 100)
