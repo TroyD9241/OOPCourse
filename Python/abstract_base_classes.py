8081b8a3f3b1bf50355d11a2c52550ca28de608f
Troy <troyd9216@gmail.com>
2021-07-21 00:26:50 -0500

    abstract

---
+++ Python/abstract_base_classes.py
@@ -0,0 +1,68 @@
+#ABC or Abstract Base Class
+
+# Abstract base classes define a set of methods
+# and properties that a class must implement
+# in order to be considered a duck-type instance of that class.
+
+class Shape(self):
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+
+    def area(self):
+        return self.length * self.length
+
+    def perimeter(self):
+        return self.length * 4
+
+shape = Shape()
+square = Square(4)
+
+from abc import ABC, abstractclassmethod, abstractmethod
+
+class ParentClass(ABC):
+    # use the decorator above the method you want to declare as abstract
+    @abstractmethod
+    def method(self)
+
+class Shape(ABC): # shape is a child class of ABC
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+
+shape = Shape()
+# this code will not compile since Shape has abstract methods without
+# method definitions in it
+
+class Shape(ABC): # shape is a child class of ABC
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+
+    def area(self):
+        return self.length * self.length
+
+    def perimeter(self):
+        return 4 * self.length
